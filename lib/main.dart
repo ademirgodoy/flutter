75b491b526f8759bb1df5cbcda982182b58bf0de
patriciapiccin <ppzanni@gmail.com>
2020-01-29 20:43:33 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.blue
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Hello Flutter"),
+          ),
+          body: Container(
+            color: Colors.white,
+          )
+      ),
+    );
+  }
+}
